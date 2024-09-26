ddb466e0787b85078dc62e58b4289735c8fc1bbe
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-09-26 08:59:19 +0530

    users fetched

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import './App.css'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 const App = () => {
+  const { data, error, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const response = await axios.get('http://localhost:3000/users');
+      return response.data;
+    }
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isSuccess) {
+    console.log("data : ", data)
+  }
+
   return (
     <div>List of users</div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,9 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>,
 )
