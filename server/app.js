67a2a4104190abe953f9cb598c4f4b0a2f249bc7
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-09-26 08:39:28 +0530

    initialized react-query

--- server/app.js
+++ server/app.js
@@ -6,6 +6,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' },
+    { id: 3, name: 'Jim' },
+    { id: 4, name: 'Jill' },
+];
+
+app.get('/users', (req,res) => {
+    res.json(users);
+});
+
+app.post('/users', (req,res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 })
